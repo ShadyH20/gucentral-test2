8ea127f492dea4dd83f15f4cffb4d8377c221665
ShadyH20 <shady.hani1@gmail.com>
2023-03-07 16:11:41 +0200

    headers

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -43,8 +43,10 @@ class _LoginPageState extends State<LoginPage> {
     try {
       var response = await http.post(url, body: body, headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://gucentrall.vercel.app',
-        'Accept': '*/*',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
       });
 
       print(response.body);
