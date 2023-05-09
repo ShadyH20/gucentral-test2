1f7f7048f2fb89aceed521c7517178366353133e
Bodia <bodyfarahat@gmail.com>
2023-05-09 10:51:08 -0700

    lambda api routes added

---
+++ lib/api/apiEndpoints.dart
@@ -0,0 +1,57 @@
+// path is lib/api/apiEndpoints
+
+// schedule
+String scheduleEndpoint =
+    "https://myn37h63ulvt7lr4gxf7k7xwfy0esrgu.lambda-url.us-east-2.on.aws/";
+
+// transcript
+String transcriptEndpoint =
+    "https://xldruxmytivrurvmmyqy7p7o6y0xzdbl.lambda-url.us-east-2.on.aws/";
+
+// check credentials
+String checkCredentialsEndpoint =
+    "https://xldruxmytivrurvmmyqy7p7o6y0xzdbl.lambda-url.us-east-2.on.aws/";
+
+// check academics to evaluate
+String checkAcademicToEvalEndpoint =
+    "https://jvsbbw67wpsc7zg3pknbwq6zya0lqmeq.lambda-url.us-east-2.on.aws/academicsToEval";
+
+// check academics evaluated
+String checkAcademicEvaluatedEndpoint =
+    "https://jvsbbw67wpsc7zg3pknbwq6zya0lqmeq.lambda-url.us-east-2.on.aws/checkAcademicEvaluated";
+
+// evaluate academics
+String evaluateAcademicEndpoint =
+    "https://jvsbbw67wpsc7zg3pknbwq6zya0lqmeq.lambda-url.us-east-2.on.aws/evaluateAcademic";
+
+// attendance
+String attendanceEndpoint =
+    "https://hr7skkl5bz3jwhmc4addmrtg5a0zjxed.lambda-url.us-east-2.on.aws/";
+
+// get id name
+String getIdNameEndpoint =
+    "https://nnlmms5lgc7jmaq2jpaqv25azi0ysogq.lambda-url.us-east-2.on.aws/";
+
+// exam schedule
+String examScheduleEndpoint =
+    "https://c6qf3femiefs4qtuhdzug3o3w40wdwln.lambda-url.us-east-2.on.aws/";
+
+// courses
+String coursesEndpoint =
+    "https://c6qf3femiefs4qtuhdzug3o3w40wdwln.lambda-url.us-east-2.on.aws/";
+
+// notifications
+String notificationsEndpoint =
+    "https://vzndq6s7ajgyk56qrrc26ji5r40lurip.lambda-url.us-east-2.on.aws/";
+
+// course grades
+String courseGradesEndpoint =
+    "https://zno2jq4rtbilseqakq4p5lajla0wmlij.lambda-url.us-east-2.on.aws/";
+
+// gpa
+String getGpaEndpoint =
+    "https://qinnaezxxl3rr4mean4qtf4vm40nogvx.lambda-url.us-east-2.on.aws/";
+
+// first login
+String firstLoginEndpoint =
+    "https://ik6lo3ue7aitmuoenwep4qzs7e0vrono.lambda-url.us-east-2.on.aws/";
